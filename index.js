68e5b112f198f4b4216b8da340bbd303eb16bd03
Muskaanpal05 <palmuskaan5@gmail.com>
2020-09-10 19:33:47 +0530

    Added animation

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ var gamePattern = [];
 var userClickedPattern = [];
 
 $(".btn").click(function(event) {
+
     var userChosenColour = $(this).attr("id");
+
     userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+
+    animatePress(userChosenColour);
 });
 
 function nextSequence() {
+
     var randomNumber = Math.floor(Math.random() * 4);
 
     var randomChosenColour = buttonColours[randomNumber];
@@ -18,8 +25,24 @@ function nextSequence() {
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+
+    animatePress(randomChosenColour);
+
+}
+
+
+
+function playSound(name) {
+
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
+}
 
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
 
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
 }
